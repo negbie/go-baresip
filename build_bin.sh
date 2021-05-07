29efe3f9cef5de9e11190bd94b1e62959477832e
negbie <negbie@posteo.de>
2021-05-07 14:00:24 +0200

    Remove echo

--- build_bin.sh
+++ build_bin.sh
@@ -4,43 +4,50 @@ set -ex
 
 #sudo apt install -y make gcc zlib1g-dev libssl-dev openssl git wget
 
-rm -rf libbaresip
-rm -f go-baresip
-mkdir libbaresip
+mkdir -p libbaresip
 cd libbaresip/
-mkdir git
-mkdir re
-mkdir rem
-mkdir baresip
+mkdir -p git
+mkdir -p re
+mkdir -p rem
+mkdir -p baresip
 cd git
 
-my_base_modules="autotest ctrl_tcp debug_cmd echo httpd menu ice stun turn uuid account contact"
+my_base_modules="autotest ctrl_tcp debug_cmd httpd menu ice stun turn uuid account contact"
 my_audio_modules="aubridge aufile auloop"
 my_codec_modules="g711"
 #my_ui_modules="stdio cons"
 my_tls_modules="srtp"
 
-git clone https://github.com/baresip/re.git
-cd re; make libre.a; cp libre.a ../../re; cd ..
 
-git clone https://github.com/baresip/rem.git
-cd rem; make librem.a; cp librem.a ../../rem; cd ..
+if [ ! -d "re" ]; then
+    git clone https://github.com/baresip/re.git
+fi
+cd re; make clean; make libre.a; cp libre.a ../../re; cd ..
 
-git clone https://github.com/baresip/baresip.git
-git clone https://github.com/baresip/baresip-apps.git
+if [ ! -d "rem" ]; then
+    git clone https://github.com/baresip/rem.git
+fi
+cd rem; make clean; make librem.a; cp librem.a ../../rem; cd ..
 
-mv baresip-apps/modules/autotest baresip/modules/
+if [ ! -d "baresip" ]; then
+    git clone https://github.com/baresip/baresip.git
+fi
+if [ ! -d "baresip-apps" ]; then
+    git clone https://github.com/baresip/baresip-apps.git
+fi
+
+cp -R baresip-apps/modules/autotest baresip/modules/
 sed -i 's/$(BARESIP_MOD_MK)/mk\/mod.mk/g' baresip/modules/autotest/module.mk
 sed -i '/auloop/a MODULES   += autotest' baresip/mk/modules.mk
 
 cd baresip
     
-make LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 libbaresip.a \
+make clean; make LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 libbaresip.a \
     MODULES="$my_base_modules $my_audio_modules $my_codec_modules $my_ui_modules $my_tls_modules"
 
 cp libbaresip.a ../../baresip; cd ..
-mv re/include ../re
-mv rem/include ../rem
-mv baresip/include ../baresip
-cd ..; rm -rf git
+cp -R re/include ../re
+cp -R rem/include ../rem
+cp -R baresip/include ../baresip
+cd ..
 
