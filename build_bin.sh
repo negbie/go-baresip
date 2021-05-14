88a64eb7863e2c511d4addfbe06175ddf770d229
negbie <negbie@posteo.de>
2021-05-14 10:42:38 +0200

    Add g722

--- build_bin.sh
+++ build_bin.sh
@@ -14,7 +14,7 @@ cd git
 
 my_base_modules="account contact autotest cons ctrl_tcp debug_cmd httpd menu ice stun turn serreg uuid stdio"
 my_audio_modules="aubridge aufile auloop"
-my_codec_modules="g711"
+my_codec_modules="g711 g722"
 my_tls_modules="srtp"
 
 
@@ -40,6 +40,8 @@ sed -i 's/$(BARESIP_MOD_MK)/mk\/mod.mk/g' baresip/modules/autotest/module.mk
 sed -i '/auloop/a MODULES   += autotest' baresip/mk/modules.mk
 
 cd baresip
+rm -rf modules/g722
+cp -ap ../../../g722 modules/
     
 make clean; make LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 libbaresip.a \
     MODULES="$my_base_modules $my_audio_modules $my_codec_modules $my_tls_modules"
